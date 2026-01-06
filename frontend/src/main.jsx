e75bcdbf059acaf81448258c024bbfc037391e28
outlife <outlifeatwork@gmail.com>
2026-01-07 03:55:09 +0530

    pwa add

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  onNeedRefresh() {
+    console.log('New content available')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  }
+})
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
